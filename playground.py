43d47fd5976e167018867ec0c635d03fee6ea4c9
m.kroll <m.kroll@robo.cash>
2023-07-09 15:18:37 +0200

    add basic playground

---
+++ playground.py
@@ -0,0 +1,14 @@
+from modules.fx import FX
+from modules.mixer import Mixer
+from modules.noise_gen import NoiseGenerator
+from modules.oscillators import Oscillator
+
+from utils.player import Player
+
+
+osc = Oscillator()
+mix = Mixer()
+fx = FX()
+ng = NoiseGenerator()
+
+player = Player()
